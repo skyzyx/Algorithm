846ce2267090157e376c9404f772c5759e07ba3f
Jie Zhang <jiez@ucsc.edu>
2015-05-22 22:43:36 -0700

    Complete merge.

--- MergeSort.java
+++ MergeSort.java
@@ -8,12 +8,12 @@ public static void main(String[] args) {
 		data[i] = ran.nextInt(1000);
 
 	System.out.println(Arrays.toString(data));
-	mergeSort(data);
+	mergeSort(data, 0, 99);
 	System.out.println(Arrays.toString(data));
 }
 
 public static void mergeSort(int[] nums, int l, int r) {
-	if (l > r)
+	if (l >= r)
 		return;
 
 	int m = (l + r) / 2;
@@ -23,5 +23,29 @@ public static void mergeSort(int[] nums, int l, int r) {
 }
 
 public static void merge(int[] nums, int l, int m, int r) {
-	
+	int n1 = m - l - 1;
+	int n2 = r - m;
+
+	int[] A = new int[n1];
+	int[] B = new int[n2];
+
+	for (int i = 0; i < n1; i++)
+		A[i] = nums[l + i];
+
+	for (int j = 0; j < n2; j++)
+		B[j] = nums[m + l + j];
+
+	int i = 0, j = 0, k = l;
+	while (i < n1 && j < n2) {
+		if (A[i] <= B[j])
+			nums[k++] = A[i++];
+		else
+			nums[k++] = B[j++]
+	}
+
+	while (i < n1)
+		nums[k++] = A[i++];
+
+	while (j < n2)
+		nums[k++] = B[j++];
 }
